33373d73c5a718b1c5277362c1b5ce2ac2ee8dc6
arkstudiodevelopers <nicksonogm@gmail.com>
2025-03-03 07:24:20 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <Form />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
---
+++ src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+    <form>
+        
+    </form>
+</template>
+
+<script>
+
+    export default{
+        name: 'Form',
+        data(){
+            return{}
+        },
+
+    }
+
+</script>
+
+<style>
+
+</style>
